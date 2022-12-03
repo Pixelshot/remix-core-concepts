dd03cba9e013f5cdf377f5b5b75b2aa898b3c2ed
pixelshot <pixelshot8@gmail.com>
2022-12-03 22:04:29 +0800

    Dynamic data with dynamic values that has dynamic routes

--- app/routes/notes.$noteId.jsx
+++ app/routes/notes.$noteId.jsx
@@ -1,5 +1,6 @@
-import { Link } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
 import styles from '~/styles/note-details.css';
+import { getStoredNotes } from '~/data/notes';
 
 // If we name this file as $noteId.jsx, the path will be at the base level. eg: localhost:300/noteId
 // But what we want instead is to have it called under notes routes: localhost:300/notes/noteId
@@ -10,19 +11,30 @@ import styles from '~/styles/note-details.css';
 // eg of dynamic path: ${note.id}
 // eg of relative path: "/note-1"
 export default function NoteDetailsPage() {
+  const selectedNote = useLoaderData();
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back to Notes</Link>
         </nav>
-        <h1>Note Title</h1>
+        <h1>{selectedNote.title}</h1>
       </header>
-      <p id="note-details-content">Note Content</p>
+      <p id="note-details-content">{selectedNote.content}</p>
     </main>
   );
 }
 
 export function links() {
   return [{ rel: 'stylesheet', href: styles }];
+}
+
+// params is also available on action()
+export async function loader({ params }) {
+  const notes = await getStoredNotes();
+  // console.log(params);
+  const selectedNote = notes.find((note) => note.id === params.noteId);
+  // The noteId part from params.noteId comes the filename that we have created
+  return selectedNote;
 }
